20c852d9ef08f9f1a38003181ac4f504cabc6bb4
Samuel Steele <steelesam72@gmail.com>
2015-02-10 14:42:40 -0500

    Update to file

--- Notes.py
+++ Notes.py
@@ -1,13 +1,24 @@
+#!/usr/bin/python
+
+## This is a demo plugin for Veer. (This demo is very dull, and has limited features). ##
+
 import pygtk
 pygtk.require('2.0')
 import gtk, cairo
 import Veer
 
-'''buff = gtk.TextBuffer()
+Frame = gtk.Frame()
+buff = gtk.TextBuffer()
 output = gtk.TextView(buffer=buff)
 output.set_left_margin(10)
 output.set_right_margin(10)
-output.set_size_request(200, 200)
+output.set_wrap_mode(gtk.WRAP_WORD_CHAR)
+
 buff.set_text("Hello World!")
 
-Nix.sidebar.add_plugin(output)'''
+Frame.set_size_request(200, 200)
+Frame.add(output)
+
+Veer.sidebar.view.attach(Frame, 0, 1, 0, 1, gtk.EXPAND, gtk.FILL, 1, 1)
+Veer.sidebar.update()
+
--- Veer.py
+++ Veer.py
@@ -12,10 +12,16 @@ class sidebar(gtk.Window):
         self.screen = self.get_screen()
         rgba = self.screen.get_rgba_colormap()
         self.set_colormap(rgba)
+    
+        self.set_vwidth(self.screen.get_height() / 4)
+        self.set_vheight(self.screen.get_height())
+        
+        self.set_size_request(20, self.get_vheight())
+        self.resize(self.get_vwidth(), self.get_vheight())
+        self.move(self.screen.get_width() - self.get_vwidth(), 25)
 
-        self.set_size_request(250, self.screen.get_height())
-
-        self.move(self.screen.get_width() - 250, 0)
+        self.view = gtk.Table(rows=self.get_vwidth(), columns=1, homogeneous=False)
+        self.add(self.view)
 
         self.connect('expose-event', self.expose)
         self.connect('enter-notify-event', self.mouse_enter)
@@ -32,20 +38,29 @@ class sidebar(gtk.Window):
         cr.set_operator(cairo.OPERATOR_OVER)
 
         cr.set_source_rgba(0.2, 0.2, 0.2, 0.575)
-        cr.rectangle(0, 0, 250, widget.screen.get_height())
+        cr.rectangle(0, 0, widget.get_vwidth(), widget.get_vheight())
         cr.fill()
 
     def mouse_enter(self, widget, event):
-        self.set_size_request(250, self.screen.get_height())
-        self.move(widget.screen.get_width() - 250, 0)
+        self.resize(self.get_vwidth(), self.get_vheight())
+        self.move(widget.screen.get_width() - self.get_vwidth(), 25)
 
     def mouse_leave(self, widget, event):
-        self.set_size_request(20, self.screen.get_height())
-        self.move(widget.screen.get_width() - 20, 0)
-	
-	def add_plugin(self, widget):
-		self.add(widget)
-		
+        self.resize(20, self.get_vheight())
+        self.move(widget.screen.get_width() - 20, 25)
+    
+    def set_vwidth(self, w):
+        self.vwidth = w
+
+    def get_vwidth(self):
+        return self.vwidth
+    
+    def set_vheight(self, h):
+        self.vheight = h
+
+    def get_vheight(self):
+        return self.vheight
+
     def update(self):
         self.show_all()
 
