23c61aa78b18af86d9931cd3ba5cc758cb6ab9d6
Samuel Steele <steelesam72@gmail.com>
2015-02-09 20:00:34 -0500

    Initial commit

---
+++ Nix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import pygtk
+pygtk.require('2.0')
+import gtk, cairo
+
+class Plugin(gtk.Widget):
+    # TODO: Create easy-to-use plugin api
+
+class Event(gtk.Event):
+    # TODO: Create events like click, scroll, 'zoom' (CTRL + and CTRL -)
